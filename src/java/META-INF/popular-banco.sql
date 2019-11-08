161245de7b33d14b67b2e09a52abca03b36b9789
Counterchase <Counterchase@DESKTOP-NUU366Q>
2019-11-07 20:31:51 -0400

    funcionando

---
+++ src/java/META-INF/popular-banco.sql
@@ -0,0 +1,16 @@
+INSERT INTO aluno (nome, rg, cpf, telefone, email, dataNascimento, endereco ) 
+VALUES ('Forsten', '999999999', '99999999999', '999999999', 'forsten@forsten.com', '1900-10-01', 'Rua Forsten');
+INSERT INTO aluno (nome, rg, cpf, telefone, email, dataNascimento, endereco ) 
+VALUES ('Bravo', '999999999', '99999999999', '999999999', 'bravo@bravo.com', '1900-10-01', 'Rua Bravo');
+INSERT INTO aluno (nome, rg, cpf, telefone, email, dataNascimento, endereco ) 
+VALUES ('Charlie', '999999999', '99999999999', '999999999', 'charlie@charlie.com', '1900-10-01', 'Rua Charlie');
+INSERT INTO aluno (nome, rg, cpf, telefone, email, dataNascimento, endereco ) 
+VALUES ('Echo', '999999999', '99999999999', '999999999', 'echo@echo.com', '1900-10-01', 'Rua Echo');
+
+INSERT INTO categoria (descricao) VALUES ('A');
+INSERT INTO categoria (descricao) VALUES ('B');
+INSERT INTO categoria (descricao) VALUES ('C');
+INSERT INTO categoria (descricao) VALUES ('D');
+INSERT INTO categoria (descricao) VALUES ('E');
+
+
